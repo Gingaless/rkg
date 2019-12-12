0e3efc4eac02ce3e3096806d4a2b035fd906a092
Gingaless <35564159+Gingaless@users.noreply.github.com>
2019-12-12 16:28:37 +0900

    Update rkg.py

--- rkg.py
+++ rkg.py
@@ -36,7 +36,7 @@ class RKG:
 
 
 		self.kdata = KianaDataSet(img_size=input_shape[0],load_from_zip=kdsfromzip,folder=folder)
-		self.gan = MyDCGAN(self.noise_size,D=self.D,G=self.G,batchsize=batchsize,print_term=print_term,Dfname = 'k_d_w.h5', Gfname='k_g_w.h5',epoch=epoch,D_optimizer=D_optimizer, G_optimizer=D_optimizer)
+		self.gan = MyDCGAN(self.noise_size,img_sizrD=self.D,G=self.G,batchsize=batchsize,print_term=print_term,Dfname = 'k_d_w.h5', Gfname='k_g_w.h5',epoch=epoch,D_optimizer=D_optimizer, G_optimizer=D_optimizer)
 		self.gan.img_data = self.kdata.normalized
 		
 		

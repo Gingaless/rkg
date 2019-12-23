60c93ba3254ce45414ee9ef90771383909016ed4
gingaless <saturniids4@gmail.com>
2019-12-23 13:26:41 +0900

    sibal

--- mywgan.py
+++ mywgan.py
@@ -86,7 +86,7 @@ class MyWGAN:
 		discriminator_output_from_generator = self.D(generated_samples_for_discriminator)
 		discriminator_output_from_real_samples =self.D(real_samples)
 		
-		averaged_samples = RandomWeightedAverage()([real_samples,generated_samples_for_discriminator])
+		averaged_samples = RandomWeightedAverage()([real_samples,generated_samples_for_discriminator], self.batch_size)
 		averaged_samples_out = self.D(averaged_samples)
 		partial_gp_loss = partial(MyWGAN.gradient_penalty_loss,averaged_samples=averaged_samples, gradient_penalty_weight=self.gradient_penalty_weight)
 		partial_gp_loss.__name__ = 'gradient_penalty'
@@ -94,9 +94,14 @@ class MyWGAN:
 		self.discriminator_model = Model(inputs=[real_samples,generator_input_for_discriminator], outputs=[discriminator_output_from_real_samples, discriminator_output_from_generator, averaged_samples_out])
 		self.discriminator_model.compile(optimizer=self.optimizer, loss=[MyWGAN.wasserstein_loss, MyWGAN.wasserstein_loss, partial_gp_loss])
 		
+	def train(self, data, print_samples=0):
 		
+		discriminator_loss, generator_loss = self.train_epoch(data, print_samples=print_samples)
 		
-	def train(self, data, epoch, print_term, print_samples=0):
+		print('average G loss : ',  np.sum(generator_loss)/self.batch_size)
+		print('average D loss : ', np.sum(discriminator_loss)/(self.batch_size*self.n_critic))
+		
+	def train_epoch(self, data, print_term=10, print_samples=0):
 		
 		np.random.shuffle(data)
 		positive_y = np.ones((self.batch_size, 1), dtype=np.float32)
@@ -108,11 +113,13 @@ class MyWGAN:
 		minibatches_size = self.batch_size*self.n_critic
 		for i in range(int(np.shape(data)[0]//minibatches_size)):
 			discriminator_minibatches = data[i*minibatches_size:(i+1)*minibatches_size]
-			for j in range(n_critic):
+			for j in range(self.n_critic):
 				image_batch = discriminator_minibatches[j*self.batch_size:(j+1)*self.batch_size]
 				noise = self.noise_generating_rule(self.batch_size, self.noise_size)
-				discriminator_loss.appens(discriminator_model.train_on_batch([image_batch, noise], [positive_y, negative_y, dummy_y]))
-			generator_loss.append(generator_model.train_on_batch(self.noise_generating_rule(self.batch_size, self.noise_size), positive_y))
+				discriminator_loss.append(self.discriminator_model.train_on_batch([image_batch, noise], [positive_y, negative_y, dummy_y]))
+			generator_loss.append(self.generator_model.train_on_batch(self.noise_generating_rule(self.batch_size, self.noise_size), positive_y))
+			
+		return discriminator_loss, generator_loss
                                             
 		
 		
@@ -124,7 +131,7 @@ class RandomWeightedAverage(_Merge):
     Inheriting from _Merge is a little messy but it was the quickest solution I could
     think of. Improvements appreciated."""
 
-    def _merge_function(self, inputs,batchsize):
+    def _merge_function(self, inputs, batchsize):
         weights = K.random_uniform((batchsize, 1, 1, 1))
         return (weights * inputs[0]) + ((1 - weights) * inputs[1])
         
